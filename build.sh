5673be4fff97a89288bdad68d4c028e836e54b95
Patrick Spek <p.spek@tyil.nl>
2018-02-07 10:08:49 +0100

    Don't use cache to build a latest

--- build.sh
+++ build.sh
@@ -5,9 +5,16 @@ main()
 	release=${1:-master}
 	tag=${1:-latest}
 
+	[ -n "$1" ] && args=" --no-cache"
+
+	shift
+
+	args="$args $*"
+
 	docker build \
 		-t "scriptkitties/perl6:$tag" \
 		--build-arg "RELEASE=$release" \
+		$args \
 		.
 }
 
